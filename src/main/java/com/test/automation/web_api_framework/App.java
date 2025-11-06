b32e237a2fe7bd61f0478867c740263b2a92678f
Yaser <yaserarafath2210gmail.com>
2025-11-07 00:42:36 +0530

    Framework creation

---
+++ src/main/java/com/test/automation/web_api_framework/App.java
@@ -0,0 +1,10 @@
+package com.test.automation.web_api_framework;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
