eea0772ec44d386edd42c084aa81b582d385c26e
Yaser <yaserarafath2210gmail.com>
2025-11-07 01:17:11 +0530

    FrameWork design

---
+++ src/test/java/com/test/automation/web_api_framework/AppTest.java
@@ -0,0 +1,19 @@
+package com.test.automation.web_api_framework;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * Unit test for simple App configured to run via TestNG suite XML.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        Assert.assertTrue(true);
+        System.out.println("Yaser");
+    }
+}
