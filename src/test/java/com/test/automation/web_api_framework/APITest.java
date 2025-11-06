12df234c68c9141abe0737411d536aa43749cb82
Yaser06 <yaserarafath2210@gmail.com>
2025-11-07 03:09:25 +0530

    .zip

--- src/test/java/com/test/automation/web_api_framework/APITest.java
+++ src/test/java/com/test/automation/web_api_framework/APITest.java
@@ -3,9 +3,7 @@ package com.test.automation.web_api_framework;
 import org.testng.annotations.*;
 
 import io.restassured.RestAssured;
-//Yaser:ATATT3xFfGF0Alp0r_t-2yhFg13jgu3w6jm_2R_QcdJru8xCC3Ix-TKYLE8XFJu07m0eZP-Jokg7L_GryP1V0TVO_-VBqiCWqjqvRIxAra6FU6jeiJ2ePfNxr3PVvGNLCK8_FaghHhFOshv1caiiTxPu868VuxeCHFvlH4UMkEzpLIFDgYbpSsw=6A094972
-// encoded:
-// WWFzZXI6QVRBVFQzeEZmR0YwQWxwMHJfdC0yeWhGZzEzamd1M3c2am1fMlJfUWNkSnJ1OHhDQzNJeC1US1lMRThYRkp1MDdtMGVaUC1Kb2tnN0xfR3J5UDFWMFRWT18tVkJxaUNXcWpxdlJJeEFyYTZGVTZqZWlKMmVQZk54cjNQVnZHTkxDSzhfRmFnaEhoRk9zaHYxY2FpaVR4UHU4NjhWdXhlQ0hGdmxINFVNa0V6cExJRkRnWWJwU3N3PTZBMDk0OTcy
+
 
 public class APITest{
 	
