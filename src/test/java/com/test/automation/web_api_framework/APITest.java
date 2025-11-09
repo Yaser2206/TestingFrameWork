42429472a79acbe51696a3dea64e09db71ecebf1
Yaser06 <yaserarafath2210@gmail.com>
2025-11-09 21:48:53 +0530

    FrameWork design

--- src/test/java/com/test/automation/web_api_framework/APITest.java
+++ src/test/java/com/test/automation/web_api_framework/APITest.java
@@ -2,18 +2,20 @@ package com.test.automation.web_api_framework;
 
 import org.testng.annotations.*;
 
-import io.restassured.RestAssured;
-
-
 public class APITest{
-	
+    
+    private String baseURI;
+
 	@BeforeTest
 	public void setup() {
-		RestAssured.baseURI="https://yaserarafath2210.atlassian.net/jira/for-you";
+		// RestAssured dependency was causing unresolved import in some environments.
+		// Use a simple string here so tests can compile without Rest Assured.
+		baseURI = "https://yaserarafath2210.atlassian.net/jira/for-you";
+		System.out.println("Base URI set to: " + baseURI);
 	}
 	
 	@Test
 	public void ValidateAPIAuthToken() {
-		System.out.println("This is a sample API test.");
+		System.out.println("This is a sample API test. Base URI: " + baseURI);
 	}
 }
